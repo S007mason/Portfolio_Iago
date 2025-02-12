4551699821aa409fe21ceb98ed5853876f2fc2b8
S007mason <iagocasascandida@gmail.com>
2025-02-12 19:22:58 +0100

    Mensaje de actualización

--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mi_app.urls')),  # Incluye las URLs de tu app
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

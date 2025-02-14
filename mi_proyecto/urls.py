0ef80f1d5cb911a23eecc9982e3489a96ec1d6ea
S007mason <iagocasascandida@gmail.com>
2025-02-14 17:34:03 +0100

    Añadido ads.txt para AdSense

--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -1,9 +1,12 @@
+import os
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mi_app.urls')),  # Incluye las URLs de tu app
+    re_path(r'^ads.txt$', serve, {'document_root': os.path.join(settings.BASE_DIR, 'static'), 'path': 'ads.txt'}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

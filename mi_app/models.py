734e4aad6d83ac69d7cb8956cfdb35f8e5778c63
S007mason <iagocasascandida@gmail.com>
2025-02-15 23:52:38 +0100

    Actualización del repositorio

--- mi_app/models.py
+++ mi_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Proyectos(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=255, default='Sin descripción')  # Valor por defecto
+    git = models.URLField(max_length=255, blank=True, null=True)
+    link = models.URLField(max_length=255, blank=True, null=True)
+    imagen = models.ImageField(upload_to='proyectos/', blank=True, null=True)  # Carpeta "proyectos/"
+
+    def __str__(self):
+        return self.nombre

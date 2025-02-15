021235d5617549c4dee0c4d9290bb9c38b07d08a
S007mason <iagocasascandida@gmail.com>
2025-02-15 23:55:57 +0100

    Actualización del repositorio

--- mi_app/views.py
+++ mi_app/views.py
@@ -8,7 +8,7 @@ from .models import Proyectos
 from django.shortcuts import render
 from .models import Proyectos
 
-def lenguajes_view(request):
+def home(request):
     proyectos = Proyectos.objects.all()
     offset = int(request.GET.get('offset', 0))  # Obtener el desplazamiento desde la URL, predeterminado a 0
     limit = 3  # Número de proyectos a mostrar por página
@@ -24,7 +24,7 @@ def lenguajes_view(request):
     offset_anterior = max(0, offset - limit)
     offset_siguiente = offset + limit
 
-    return render(request, 'mi_app/index.html', {
+    return render(request, 'portfolio/index.html', {
         'proyectos_visible': proyectos_visible,
         'hay_anterior': hay_anterior,
         'hay_siguiente': hay_siguiente,

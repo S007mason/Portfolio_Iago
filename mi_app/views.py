4569af5077acc6db2e14b7330d6e03117d58a323
S007mason <iagocasascandida@gmail.com>
2025-02-16 00:10:07 +0100

    Actualización del repositorio

--- mi_app/views.py
+++ mi_app/views.py
@@ -1,33 +1,4 @@
-from django.core.paginator import Paginator
-from django.http import JsonResponse
-from django.shortcuts import render
-from .models import Proyectos
-
-
-
-from django.shortcuts import render
-from .models import Proyectos
+from django.shortcuts import render  # Reemplaza la importación de HttpResponse
 
 def home(request):
-    proyectos = Proyectos.objects.all()
-    offset = int(request.GET.get('offset', 0))  # Obtener el desplazamiento desde la URL, predeterminado a 0
-    limit = 3  # Número de proyectos a mostrar por página
-
-    proyectos_visible = proyectos[offset:offset + limit]  # Seleccionar el subconjunto de proyectos
-    total_proyectos = proyectos.count()  # Contar todos los proyectos
-
-    # Calcular si hay más elementos a la derecha o izquierda
-    hay_anterior = offset > 0
-    hay_siguiente = offset + limit < total_proyectos
-
-    # Calcular los nuevos offsets
-    offset_anterior = max(0, offset - limit)
-    offset_siguiente = offset + limit
-
-    return render(request, 'mi_app/index.html', {
-        'proyectos_visible': proyectos_visible,
-        'hay_anterior': hay_anterior,
-        'hay_siguiente': hay_siguiente,
-        'offset_anterior': offset_anterior,
-        'offset_siguiente': offset_siguiente,
-    })
+    return render(request, 'mi_app/index.html')  # Renderiza la plantilla

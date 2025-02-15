f30d895c64c326e45788f4726eb64bbd4601eb80
S007mason <iagocasascandida@gmail.com>
2025-02-16 00:25:26 +0100

    Actualización del repositorio

--- mi_app/views.py
+++ mi_app/views.py
@@ -3,4 +3,9 @@ from .models import Proyectos
 
 def home(request):
     proyectos_visible = Proyectos.objects.all()  # Obtiene todos los proyectos
+
+    # Si no hay proyectos, se envía None o un mensaje vacío
+    if not proyectos_visible.exists():
+        proyectos_visible = None  # También puedes enviar []
+
     return render(request, 'mi_app/index.html', {'proyectos_visible': proyectos_visible})
